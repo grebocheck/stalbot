0e5bed808851309f0afcfd8f9e180aca7772790a
vadim <@grebocheck>
2023-01-26 20:11:16 +0200

    upd logs

--- additions/log.py
+++ additions/log.py
@@ -10,13 +10,13 @@ colorama.init()
 def log_err(a):
     today = datetime.datetime.today()
     t = "[" + today.strftime("%H:%M:%S - %d.%m.%y") + "] " + a
-    err = io.open('logs/logs/err.txt', "a", encoding="utf-8")
+    err = io.open('logs/err.txt', "a", encoding="utf-8")
     err.write(t + '\n')
     err.close()
-    inf = io.open("logs/logs/inf.txt", "a", encoding="utf-8")
+    inf = io.open("logs/inf.txt", "a", encoding="utf-8")
     inf.write(t + '\n')
     inf.close()
-    deb = io.open("logs/logs/deb.txt", "a", encoding="utf-8")
+    deb = io.open("logs/deb.txt", "a", encoding="utf-8")
     deb.write(t + '\n')
     deb.close()
     print(Fore.RED + "[" + today.strftime("%H:%M:%S - %d.%m.%y") + "] " + Fore.WHITE + a)
@@ -25,10 +25,10 @@ def log_err(a):
 def log_inf(a):
     today = datetime.datetime.today()
     t = "[" + today.strftime("%H:%M:%S - %d.%m.%y") + "] " + a
-    inf = io.open("logs/logs/inf.txt", "a", encoding="utf-8")
+    inf = io.open("logs/inf.txt", "a", encoding="utf-8")
     inf.write(t + '\n')
     inf.close()
-    deb = io.open("logs/logs/deb.txt", "a", encoding="utf-8")
+    deb = io.open("logs/deb.txt", "a", encoding="utf-8")
     deb.write(t + '\n')
     deb.close()
     print(Fore.YELLOW + "[" + today.strftime("%H:%M:%S - %d.%m.%y") + "] " + Fore.WHITE + a)
@@ -37,7 +37,7 @@ def log_inf(a):
 def log_deb(a):
     today = datetime.datetime.today()
     t = "[" + today.strftime("%H:%M:%S - %d.%m.%y") + "] " + a
-    deb = io.open("logs/logs/deb.txt", "a", encoding="utf-8")
+    deb = io.open("logs/deb.txt", "a", encoding="utf-8")
     deb.write(t + '\n')
     deb.close()
     print(Fore.GREEN + "[" + today.strftime("%H:%M:%S - %d.%m.%y") + "] " + Fore.WHITE + a)
