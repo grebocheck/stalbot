3e29f1975cdae98963ee5ae06014a847daf335ae
vadim <@grebocheck>
2023-01-29 19:13:32 +0200

    Ручной морж

--- branches/cur_price.py
+++ branches/cur_price.py
@@ -21,12 +21,18 @@ async def process_price_two(message: types.Message, state: FSMContext):
         await state.finish()
         image_path = dbitem.get_item_image(my_item_id=it_item, server_name=user_server)
         item_name = dbitem.search_item_name_by_id(it_item, server_name=user_server, lang=user_lang)
-        plot = await worse.get_auc_lot(item_id=it_item, server=user_server,
-                                       lang=user_lang, image_path=image_path, item_name=item_name)
+        plot, back_btn, next_btn = await worse.get_auc_lot(item_id=it_item, server=user_server,
+                                                           lang=user_lang, image_path=image_path,
+                                                           page=0, item_name=item_name)
         if plot:
-            return await bot.send_photo(user.id, plot, caption=await lng.trans("Цены на аукционе сервера {} сейчас на предмет {} ⚖", user,
-                                                                    [user_server, item_name]),
-                                    parse_mode="Markdown", reply_markup=await get_main_keyboard(user))
+            keyboard = await get_cur_price_keyboard(next_btn=next_btn, back_btn=back_btn, page=0, item=it_item)
+            await message.reply_photo(plot, caption=await lng.trans(
+                "Цены на аукционе сервера {} сейчас на предмет {} ⚖", user,
+                [user_server, item_name]),
+                                      parse_mode="Markdown", reply_markup=keyboard)
+            plot.close()
+            os.remove("table.png")
+            return
         else:
             return await message.reply(await lng.trans("На аукционе нет лотов для товара: {}", user, item_name))
     await message.reply(await lng.trans("Простите, но я не могу найти этот предмет😰", user),
@@ -48,10 +54,13 @@ async def cnange_emission_callback(callback: types.CallbackQuery):
     image_path = dbitem.get_item_image(my_item_id=it_item, server_name=user_server)
     item_name = dbitem.search_item_name_by_id(it_item, server_name=user_server, lang=user_lang)
     plot, back_btn, next_btn = await worse.get_auc_lot(item_id=it_item, server=user_server,
-                                                       lang=user_lang, image_path=image_path, page=page)
+                                                       lang=user_lang, image_path=image_path,
+                                                       page=page, item_name=item_name)
     if plot:
         keyboard = await get_cur_price_keyboard(next_btn=next_btn, back_btn=back_btn, page=page, item=it_item)
-        await callback.message.edit_media(media=types.InputMediaPhoto(plot),
+        text = await lng.trans(
+            "Цены на аукционе сервера {} сейчас на предмет {} ⚖", user, [user_server, item_name])
+        await callback.message.edit_media(media=types.InputMediaPhoto(plot, caption=text),
                                           reply_markup=keyboard)
         plot.close()
         os.remove("table.png")
